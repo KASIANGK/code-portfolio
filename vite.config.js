9ccbaf177bf3c32b5bbbc282d2afff7c9384cde3
kiss_my_fire <ngk.kasia@gmail.com>
2024-12-03 01:19:56 +0100

    Mettre à jour .gitignore et corriger les fichiers de build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      axios: 'node_modules/axios/dist/axios.min.js',
-      // autres alias si nécessaire
-    }
-  },
-  plugins: [
-    // Autres plugins nécessaires pour Vite
-  ]
+  build: {
+    assetsDir: 'assets',  // s'assurer que le dossier assets est correctement configuré
+  }
 });
+
