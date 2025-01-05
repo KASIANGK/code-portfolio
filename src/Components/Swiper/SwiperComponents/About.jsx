ce56d7ce605eb802f3a47d10ae4c748b7e91b3ed
kiss_my_fire <ngk.kasia@gmail.com>
2025-01-05 11:59:39 +0100

    up img

--- src/Components/Swiper/SwiperComponents/About.jsx
+++ src/Components/Swiper/SwiperComponents/About.jsx
@@ -40,7 +40,7 @@ function About() {
                 autoPlay
                 muted
               >
-                <source src={lightModeVideooo} type="video/mp4" />
+                <source src="/assets/aboutgrain.mp4" type="video/mp4" />
                 Votre navigateur ne supporte pas la balise vidéo.
               </video>
             </div>
